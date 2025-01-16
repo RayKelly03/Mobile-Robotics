df8945a92b2016f89bbf0ef8da760973f34d2c0f
RayKelly03 <raymond.kelly228@mail.dcu.ie>
2025-01-16 09:57:07 +0000

    Update LineFollowingSensors.ino

--- LineFollowingSensors.ino
+++ LineFollowingSensors.ino
@@ -1,9 +1,21 @@
 int AnalogValue[5] = {0,0,0,0,0};
 int AnalogPin[5] = {4,5,6,7,15}; // keep 8 free for tone O/P music
 
+int motor1PWM = 37;
+int motor1Phase = 38;
+int motor2PWM = 39;
+int motor2Phase = 20;
+
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(9600);
+    pinMode(motor1PWM,OUTPUT);
+    pinMode(motor1Phase,OUTPUT);
+    pinMode(motor2PWM,OUTPUT);
+    pinMode(motor2Phase,OUTPUT);
+    for (int i=0;i<5;i++) {
+      pinMode(AnalogPin[i] , INPUT) ;
+    }
 } 
 
 void loop(){
@@ -19,4 +31,36 @@ void loop(){
       delay(600); // display new set of readings every 600mS
     }
   }
-} 
+  while (AnalogValue[2] < 250) {
+    Forward();
+    AnalogValue[2]=analogRead(AnalogPin[2]); //middle Sensor
+  }
+  while (AnalogValue[1] < 250) {
+    Left();
+    AnalogValue[1]=analogRead(AnalogPin[1]); //Left Sensor
+  }
+
+  while (AnalogValue[3] < 250) {
+    Right();
+    AnalogValue[3]=analogRead(AnalogPin[3]); //Left Sensor
+  }
+}
+void Forward() {
+  digitalWrite(motor1Phase, HIGH); //forward
+  digitalWrite(motor2Phase, HIGH); 
+  analogWrite(motor1PWM, 100); // set speed of motor
+  analogWrite(motor2PWM, 100); // set speed of motor
+  Serial.println("Forward"); // Display motor direction
+}
+
+void Right() {
+  digitalWrite(motor1Phase, HIGH); 
+  analogWrite(motor1PWM, 100); // set speed of motor
+  Serial.println("Right"); // Display motor direction
+}
+
+void Left() {
+  digitalWrite(motor2Phase, HIGH); 
+  analogWrite(motor2PWM, 100); // set speed of motor
+  Serial.println("Left"); // Display motor direction
+}
