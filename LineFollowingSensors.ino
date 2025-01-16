455c10e16d4a281083fc51614fcc49fb12a01006
RayKelly03 <raymond.kelly228@mail.dcu.ie>
2025-01-16 10:52:42 +0000

    Update LineFollowingSensors.ino

--- LineFollowingSensors.ino
+++ LineFollowingSensors.ino
@@ -21,12 +21,12 @@ void setup() {
 void loop(){
   // put your main code here, to run repeatedly:
   int i;
-  for (i=0;i<5;i++) {
+  for (i=1;i<4;i++) {
     AnalogValue[i]=analogRead(AnalogPin[i]);
 
     Serial.print(AnalogValue[i]); // This prints the actual analog reading from the sensors
     Serial.print("\t"); //tab over on screen
-    if(i==4) {
+    if(i==3) {
       Serial.println(""); //carriage return
       delay(600); // display new set of readings every 600mS
     }
@@ -35,14 +35,27 @@ void loop(){
     Forward();
     AnalogValue[2]=analogRead(AnalogPin[2]); //middle Sensor
   }
+  while (AnalogValue[2] > 250) {
+    Stop(motor1PWM);
+    Stop(motor2PWM);
+    AnalogValue[2]=analogRead(AnalogPin[2]); //middle Sensor
+  }
   while (AnalogValue[1] < 250) {
     Left();
+    AnalogValue[1]=analogRead(AnalogPin[1]); //Left Sensor
+  }
+  while (AnalogValue[1] > 250) {
+    Stop(motor2PWM);
     AnalogValue[1]=analogRead(AnalogPin[1]); //Left Sensor
   }
 
   while (AnalogValue[3] < 250) {
     Right();
-    AnalogValue[3]=analogRead(AnalogPin[3]); //Left Sensor
+    AnalogValue[3]=analogRead(AnalogPin[3]); //Right Sensor
+  }
+  while (AnalogValue[3] > 250) {
+    Stop(motor1PWM);
+    AnalogValue[1]=analogRead(AnalogPin[1]); //Left Sensor
   }
 }
 void Forward() {
@@ -63,4 +76,9 @@ void Left() {
   digitalWrite(motor2Phase, HIGH); 
   analogWrite(motor2PWM, 100); // set speed of motor
   Serial.println("Left"); // Display motor direction
+}
+
+void Stop(int MotorPwm) {
+  analogWrite(MotorPwm, 0); 
+  Serial.println("Stop");
 }
