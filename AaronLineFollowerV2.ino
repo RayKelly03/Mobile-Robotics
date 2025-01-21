47e0440afec60e8e10687a79e06f585000355545
Aaron <163346151+dodayron@users.noreply.github.com>
2025-01-21 10:39:56 +0000

    error correction AaronLineFollowerV2.ino

--- AaronLineFollowerV2.ino
+++ AaronLineFollowerV2.ino
@@ -1,103 +1,81 @@
 #include <DRV8835MotorShield.h>
 
-//pin definitions
+// Pin definitions
 #define LED_PIN 39
 #define M1PWM 37
 #define M1Phase 38
 #define M2PWM 39  
 #define M2Phase 20
 
-//motor driver library call
+// Motor driver library call
 DRV8835MotorShield motors(M1Phase, M1PWM, M2Phase, M2PWM);
 
-//variables
-int minValues[5]
-int max0Values[5]
-int threshold[5]
+// Variables
+int minValues[5] = {0};
+int maxValues[5] = {0};
+int threshold[5] = {0};
 
-//pin assignments
+// Pin assignments
 int pins[] = {4, 5, 6, 7, 15}; // Define the pins to be calibrated
 int numPins = 5; // Get the number of pins
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
 
-
-  //assign pin 
-  for (int i = 0; i < numPins; i++){
+  // Assign initial values to min and max
+  for (int i = 0; i < numPins; i++) {
     minValues[i] = analogRead(pins[i]);
     maxValues[i] = analogRead(pins[i]);
   }
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
   calibrate();
   delay(1000);
-  //maybe set up a button input after calibration to activate line following
 
-  while(){
-
-    //case for a sharp left turn or right turn
-    if (analogRead(pins[1]) < threshold[1] && analogRead(pins[5]) > threshold[5] )
-    {
-      motors.setM1Speed(0)
-      motors.setM2Speed(200)
-    }
-    else if (analogRead(pins[1]) > threshold[1] && analogRead(pins[5]) < threshold[5] )
-    {
-      motors.setM1Speed(200)
-      motors.setM2Speed(0)
+  while (true) {
+    // Case for a sharp left turn or right turn
+    if (analogRead(pins[1]) < threshold[1] && analogRead(pins[4]) > threshold[4]) {
+      motors.setM1Speed(0);
+      motors.setM2Speed(200);
+    } else if (analogRead(pins[1]) > threshold[1] && analogRead(pins[4]) < threshold[4]) {
+      motors.setM1Speed(200);
+      motors.setM2Speed(0);
     }
 
-    //regular line follow using PID or otherwise
-    else if (analogRead(pins[3]) < threshold[3]){
-      pid();// to be completed
+    // Regular line follow using PID or otherwise
+    else if (analogRead(pins[3]) < threshold[3]) {
+      pid(); // To be implemented
     }
-
-
   }
-
 }
 
-//to be done later 
-void pid(){
-
+// To be done later
+void pid() {
+  // Placeholder for PID logic
 }
 
-
-
-//spins the bot, to scan the area underneath to measure the white and black 
-//so it can automatically select the most appropriate threshold value to 
-//differentiate between black and white.
+// Spins the bot to calibrate thresholds for white and black surfaces
 void calibrate() {
-
-  // Initialize min and max values for the specified pins
-  // moved to set up
-
   // Run the motors and calibrate min and max values
-  for (int i = 0; i < 3000; i++){
-    motors.setM1Speed(100)
-    motors.setM2Speed(-100)
-    
-    for (int j = 0; j < numPins; j++){
+  for (int i = 0; i < 3000; i++) {
+    motors.setM1Speed(100);
+    motors.setM2Speed(-100);
+
+    for (int j = 0; j < numPins; j++) {
       int value = analogRead(pins[j]);
-      if (value < minValues[j])
-      {
+      if (value < minValues[j]) {
         minValues[j] = value;
       }
-      if (value > maxValues[j])
-      {
+      if (value > maxValues[j]) {
         maxValues[j] = value;
       }
     }
   }
 
   // Calculate thresholds and print them
-  for (int i = 0; i < numPins; i++){
+  for (int i = 0; i < numPins; i++) {
     threshold[i] = (minValues[i] + maxValues[i]) / 2;
     Serial.print(threshold[i]);
     Serial.print("   ");
@@ -105,6 +83,6 @@ void calibrate() {
   Serial.println();
 
   // Stop the motors
-  motors.setM1Speed(0)
-  motors.setM2Speed(0)
+  motors.setM1Speed(0);
+  motors.setM2Speed(0);
 }
