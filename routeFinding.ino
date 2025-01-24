de9fe749fde9d11b336b781367a3934f96d39da8
matthew-hoconnor <matthew.horneoconnor2@mail.dcu.ie>
2025-01-24 18:11:52 +0000

    Create routeFinding.ino

---
+++ routeFinding.ino
@@ -0,0 +1,55 @@
+// Node A = Node 5, Node B = Node 6
+
+class Graph {
+  private: 
+    int adjMatrix[7][7];
+    
+  public:
+    Graph() {
+      
+        for (int i = 0; i < 7; ++i) {
+            for (int j = 0; j < 7; ++j) {
+                adjMatrix[i][j] = 0;
+            }
+        }
+    }
+
+    void addEdge (unsigned int v, unsigned int w, int weight) {
+      if (v < 7 && w < 7) {
+        adjMatrix[v][w] = weight;
+      }
+      else {
+            Serial.println("Error: Vertex out of bounds!"); // Inform the user
+        }
+    }
+
+
+    void displayMatrix() {
+        for (int i = 0; i < 7; ++i) {
+            for (int j = 0; j < 7; ++j) {
+                Serial.print(adjMatrix[i][j]);
+                Serial.print(" ");
+            }
+            Serial.println();
+        }
+    }
+};
+
+
+//void dijkstra()
+void setup() {
+    Serial.begin(9600);
+    Graph g;
+    g.addEdge(0, 1, 4);
+    g.addEdge(1, 2, 8);
+    g.addEdge(6, 5, 10); // Valid edge
+    g.addEdge(7, 0, 12); // Out of bounds edge
+
+    Serial.println("Adjacency Matrix:\n");
+    g.displayMatrix();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
